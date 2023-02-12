6ae4ac5fe803587af94296654f7edefa30a48b2c
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-02-11 19:40:30 -0600

    Added fixtures and more model attributes

--- ccapi/models/__init__.py
+++ ccapi/models/__init__.py
@@ -1,4 +1,7 @@
+"""models package"""
 from .user import User
 from .tech import Tech
 from .learned_tech import LearnedTech
 from .goal import Goal
+from .message import Message
+from .topic import Topic
--- ccapi/models/topic.py
+++ ccapi/models/topic.py
@@ -2,7 +2,6 @@
 import uuid
 from django.db import models
 from .user import User
-from .tech import Tech
 from .learned_tech import LearnedTech
 from .goal import Goal
 
@@ -13,6 +12,6 @@ class Topic(models.Model):
     description = models.CharField(max_length=150)
     uid = models.ForeignKey(User, on_delete= models.RESTRICT)
     learned_tech = models.ForeignKey(LearnedTech, on_delete= models.PROTECT)
-    goal = models.ForeignKey(Goal, null=True)
+    goal = models.ForeignKey(Goal, on_delete=models.RESTRICT, null=True)
     last_updated = models.DateTimeField(auto_now=True)
     completed = models.BooleanField(default=False)
