5fc307becc6bddfbe7160933df7b2332fb6e7bdb
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-02-13 10:02:14 -0600

    Added Post method to User, set up url

--- cc/urls.py
+++ cc/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ccapi.views import register_user, check_user
 
 urlpatterns = [
+    path('register', register_user),
+    path('checkuser', check_user),
     path('admin/', admin.site.urls),
 ]
