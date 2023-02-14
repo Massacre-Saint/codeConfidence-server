dad67cef80449bba612ab5e324b2070a41afa5c3
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-02-13 19:14:37 -0600

    Added User Viewset/urls

--- cc/urls.py
+++ cc/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ccapi.views import register_user, check_user
+from django.conf.urls import include
+from rest_framework import routers
+from ccapi.views import register_user, check_user, MessageView
 
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'messages', MessageView, 'messages')
 urlpatterns = [
     path('register', register_user),
     path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
---
+++ ccapi/views/message.py
@@ -0,0 +1,28 @@
+"""Module sets up Django Viewset for the class of Message"""
+from random import randrange
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from ccapi.models import Message
+
+class MessageView(ViewSet):
+    """Class creates the Viewset for class Message"""
+
+    def list(self, request):
+        """Method retrieves single message instance"""
+        count = Message.objects.count()
+        random_index = randrange(count)
+        random_message = Message.objects.all()[random_index]
+        serializer = MessageSerializer(random_message)
+
+        return Response(serializer.data)
+class MessageSerializer(serializers.ModelSerializer):
+    """Class creates the serializer for Message class"""
+
+    class Meta:
+        model = Message
+        fields = (
+          'id',
+          'title',
+          'author'
+          )
