7acaa5de78e91860dd141bf690422f39b5d0aec9
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-03-16 21:44:18 -0500

    Deplyed to Heroku

--- cc/settings.py
+++ cc/settings.py
@@ -1,3 +1,5 @@
+
+import os
 """
 Django settings for cc project.
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bz-43evi!y=lj2)egm2!s(@qcrwavug)suf@9@wo2rf&teyq+5'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
