0c5e62b53f65fb7950dd3c6c99d9b67782c82bbc
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-03-16 21:54:39 -0500

    Added setup files for deployment

--- cc/settings.py
+++ cc/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import django_on_heroku
 """
 Django settings for cc project.
 
@@ -137,3 +138,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
