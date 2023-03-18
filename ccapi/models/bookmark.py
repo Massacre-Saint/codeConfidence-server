2a1559d23b73179530066b90548310f57f9f26b4
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-03-18 12:57:21 -0500

    Added resource model

--- ccapi/models/bookmark.py
+++ ccapi/models/bookmark.py
@@ -1,11 +1,10 @@
 """Module creates Django Model for BookMark"""
 from django.db import models
-from .user import User
 
 class Bookmark(models.Model):
     """Create class for Bookmark Model"""
     id= models.IntegerField(primary_key=True)
     index = models.PositiveIntegerField()
     parent_id = models.PositiveIntegerField()
-    title = models.CharField(max_length=25)
+    title = models.CharField(max_length=150)
     url = models.CharField(max_length=100, null=True)
