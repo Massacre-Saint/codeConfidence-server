ccc907581dccb558a88e075d8139b2c0331e57de
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-07-11 19:29:25 -0500

    Added server to cors list

--- cc/settings.py
+++ cc/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://code-confidence-server.herokuapp.com/',
+    'https://code-confidence-server.herokuapp.com',
     'https://code-confidence.netlify.app',
 )
 CORS_ALLOWED_ORIGINS = [
